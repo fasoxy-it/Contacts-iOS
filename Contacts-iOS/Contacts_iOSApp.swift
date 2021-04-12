0dd962b28e81f11e5b949105e73c3608319621ca
fasoxy-it <fasoxy@gmail.com>
2021-04-12 15:44:42 +0200

    Initial Commit

---
+++ Contacts-iOS/Contacts_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Contacts_iOSApp.swift
+//  Contacts-iOS
+//
+//  Created by Mattia Fasoli on 12/04/21.
+//
+
+import SwiftUI
+
+@main
+struct Contacts_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
